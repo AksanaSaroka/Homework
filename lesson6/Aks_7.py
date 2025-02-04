4623b2f1e43cff1060cbfab3992748bfd44e3e47
Aksana <aksana.zholtko@mail.ru>
2025-02-04 19:13:35 +0300

    lesson6 недоделанное 5,6

--- lesson6/Aks_7.py
+++ lesson6/Aks_7.py
@@ -3,3 +3,21 @@
 запрашивала город а в ответ выдавала подробную информацию 
 о погоде в этом городе в красивом формате.
 """
+
+
+
+from pyowm import OWM
+from pprint import pprint
+
+owm = OWM('87b7c330daf2579649274a648d33f753')
+mgr = owm.weather_manager()
+
+# print(dir(str))
+# print(dir(mgr))
+city = input('Введите город: ')
+obs = mgr.weather_at_place(city)
+w = obs.to_dict()
+
+
+
+pprint(w['weather'])
