1ea95592389cd359962ee71a1645ab36cb46f7a7
Aksana <aksana.zholtko@mail.ru>
2025-02-06 17:31:33 +0300

    less7task3

--- lesson7/Aks_7_3.py
+++ lesson7/Aks_7_3.py
@@ -3,3 +3,16 @@
 у которой номер в алфавите равен этой цифре. 
 Например: 1352=aceb.
 '''
+alf = "abcdefghijklmnopqrstuvwxyz"
+
+number = input('Введите число: ')
+
+for a in number:
+     number = number.replace(a, alf[int(a)-1])
+         
+             
+print(number)
+
+
+
+
