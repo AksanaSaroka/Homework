8fb4955ffe37f769c011d2a771b736432c4357f3
Aksana <aksana.zholtko@mail.ru>
2025-02-11 19:02:34 +0300

    less8task5(недоконца)

--- lesson8/Aks_8_5.py
+++ lesson8/Aks_8_5.py
@@ -5,3 +5,16 @@
 Нельзя пользоваться collections.Counter!
 
 '''
+text = 'Магазин игрушек на Немиге'
+
+def count_char(text:str):
+
+    d = {}
+    count = 0
+    for symbol in text:
+        if symbol:
+            count += 1
+            d.update({symbol: count})
+    return(d)
+
+print(count_char(text))
