9d43ae083348ca4b8420e631cc9ef6ba7fedf608
Aksana <aksana.zholtko@mail.ru>
2025-02-14 16:57:28 +0300

    less7task7.9(changed)

--- lesson8/Aks_8_5.py
+++ lesson8/Aks_8_5.py
@@ -12,9 +12,9 @@ def count_char(text:str):
     d = {}
     count = 0
     for symbol in text:
-        if symbol:
-            count += 1
-            d.update({symbol: count})
+            if symbol:
+                count += 1
+                d.update({symbol: count})
     return(d)
 
 print(count_char(text))
