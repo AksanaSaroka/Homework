b48d492392ab70d3c9d42110a93feb2311987e9f
Aksana <aksana.zholtko@mail.ru>
2025-02-18 17:11:42 +0300

    less9task6,8

--- lesson9/Aks_9_6.py
+++ lesson9/Aks_9_6.py
@@ -4,3 +4,9 @@
 Отсортировать словарь по температуре в порядке возрастания и обратно.
 
 """
+d = {"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}
+
+b = dict(sorted(d.items(), key = lambda x: x[1], reverse = False))
+c = dict(sorted(d.items(), key = lambda x: x[1], reverse = True))
+print(b)
+print(c)
--- lesson9/Aks_9_8.py
+++ lesson9/Aks_9_8.py
@@ -5,3 +5,7 @@
  - остался только логический тип.
  
 '''
+s = ['a', 'b', 4, True, 'Hello', False, 0.25]
+
+print(list(filter(lambda x:isinstance(x,str), s)))
+print(list(filter(lambda x:isinstance(x,bool), s)))
