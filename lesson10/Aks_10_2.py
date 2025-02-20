7a742ebe85ddd91d315e21946ebef127c0cb5051
Aksana <aksana.zholtko@mail.ru>
2025-02-20 14:57:53 +0300

    less10task2,3,4,6

--- lesson10/Aks_10_2.py
+++ lesson10/Aks_10_2.py
@@ -11,3 +11,19 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+def factorial(a):
+    if a > 1:
+        return (factorial(a-1)*a)
+    elif 0 <= a <= 1:
+        return 1
+    else:
+        return 'err: факториал не может быть отрицательным числом '
+
+def factorial_gen(n):
+    for i in range(1,n+1):
+        yield factorial(i)
+
+
+
+print(next(factorial_gen(5)))
+print(next(factorial_gen(5)))
