4b68cc5c3d4013f801c94a4e488fac742a6cb193
Aksana <aksana.zholtko@mail.ru>
2025-03-03 13:35:05 +0300

    less12task3

--- lesson12/Aks_12_3.py
+++ lesson12/Aks_12_3.py
@@ -21,3 +21,66 @@
     * - stat, возвращает среднее количество изменений счетчика в секунду
 
 """
+class Counter:
+    text_err = "Выход за пределы диапазона"
+
+    def __init__(self, start=0, value=0, end=1):
+        self.start = start
+        self.value = value
+        self.end = end
+
+    def __increase__(self):
+        if  self.start <= self.value < self.end:
+            self.value += 1
+            return self.value
+        else:
+            return self.text_err
+        
+        
+
+    
+    def __decrease__(self):
+        if  self.start < self.value <= self.end:
+            self.value -= 1
+            return self.value
+        else:
+            return self.text_err
+
+    def __reset__(self):
+        return self.start
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if  self.start <= self.value <= self.end:
+            value = self.value
+            self.value += 1
+            return value
+        else:
+            return self.text_err
+
+        
+
+a = Counter(10,11,15)
+print('(1)',a.__increase__())
+print('(2)',a.__increase__())
+print('(3)',a.__decrease__())
+print(a.__reset__())
+
+print('(4)', a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+
+b = Counter()
+print('(1)',b.__increase__())
+print('(2)',b.__increase__())
+print('(3)',b.__decrease__())
+print(b.__reset__())
+print('(4)', b.__next__())
+print(b.__next__())
+print(b.__next__())
