042d3bed2983dddccbeb8874b9c40b380c264f3a
Aksana <aksana.zholtko@mail.ru>
2025-03-05 15:17:17 +0300

    13.3(3!!)

--- lesson13/Aks_hero1.py
+++ lesson13/Aks_hero1.py
@@ -1,4 +1,4 @@
-from random import Random 
+import random
 
 class Hero:
     """     
@@ -61,30 +61,48 @@ class Spec_Attac:
 
     def special_attack(self, other):
         """
-        метод атаки special_attack которая возможна только если количество 
+        метод атаки special_attack, которая возможна только если количество 
                 спец.очков более 0.
         """  
         if self.unique_power > 0:
             other.damage -= self.unique_power
             self.unique_power -= 1
-            return (f"Применена специальная атака.Уникальная сила: {self.unique_power} спец. очков.Урон противника:{other.damage}")
+            print (f"""
+                   {self.name} применил специальную атаку против {other.name}.\n
+                   Уникальная сила {self.name}: {self.unique_power} спец. очков. \n
+                   Урон {other.name}:{other.damage} спец. очков.
+                   """)
         else:
-            print(f"Уникальное свойство истощено: нет спец.очков") 
+            print( f"Уникальное свойство истощено: нет спец.очков")
 
-    def choise_of_attack(self,usual_attack):
+
+    def do_attack(self,other):
         """
-        Метод attack который при атаке с вероятностью 25% будет использовать 
-        спец.способность героя если у него остались спец.очки. 
+        Метод attack, который при атаке с вероятностью 25% использует 
+        спец.способность героя, если у него остались спец.очки. 
         При спец атаке вычитать из очков 1. Если вероятность пришлась на
-        остальные 75% - выполнить обычную атаку. Вывести сообщение в консоль 
+        остальные 75% - выполняется обычную атаку. Выводит сообщение в консоль 
         о типе и результате атаки
         """
+        if self.unique_power > 0  and random.randint(1,100) <= 25:
+            self.special_attack(other)
+        else:
+            self.func_attack(other)
+            print (f"""
+                    {self.name} применил обычную атаку против {other.name}.\n
+                   {self.name}: состояние защиты:{self.armor}, состояние здоровья: {self.health}. \n
+                   {other.name}: состояние защиты:{other.armor}, состояние здоровья: {other.health}. 
+                   """)
+           
+        
+    def func_attack(self,other):
+        pass    
 
-        self.special_attack() if self.unique_power > 0  and random.randint(1,100) <= 25 else usual_attack()
 
     def __str__(self):
         return f'{self.name}'
     
+
     def __repr__(self):
         return f'{self.name}'
 
@@ -102,10 +120,17 @@ class Mag(Hero, Spec_Attac):
         
    
     def print_info(self, sep="-"):
-        info =  f"{Hero._get_info(self)}\n"  \
+        info =  f"{__class__.__name__}\n"   \
+                f"{Hero._get_info(self)}\n"  \
                 f"{sep*20}\n" \
                 f"Mana - {self.unique_power}\n"
         print(info)
+
+    def do_attack(self,other):
+        Spec_Attac.func_attack = Hero.kick
+        Spec_Attac.do_attack(self,other)
+
+        
 
 # орки(ярость)
 class Orc(Hero, Spec_Attac):   
@@ -117,13 +142,21 @@ class Orc(Hero, Spec_Attac):
        
    
     def print_info(self, sep="-"):
-        info =  f"{Hero._get_info(self)}\n"  \
+        info =  f"{__class__.__name__}\n"   \
+                f"{Hero._get_info(self)}\n"  \
                 f"{sep*20}\n" \
                 f"Ярость - {self.unique_power}\n"
         print(info)
+    
+    def do_attack(self,other):
+        Spec_Attac.func_attack = Hero.kick
+        Spec_Attac.do_attack(self,other)
+
+
 
 # гномы(выносливость)
-class Dwarf(Hero, Spec_Attac):    
+class Dwarf(Hero, Spec_Attac): 
+
     def __init__(self, name, health, armor, strong,  unique_power, damage) -> None:
         # Hero.__init__(self, name, health, armor, strong)
         Hero.__init__(self, name, health, armor, strong)
@@ -131,13 +164,20 @@ class Dwarf(Hero, Spec_Attac):
        
    
     def print_info(self, sep="-"):
-        info =  f"{Hero._get_info(self)}\n"  \
+        info =  f"{__class__.__name__}\n"   \
+                f"{Hero._get_info(self)}\n"  \
                 f"{sep*20}\n" \
                 f"Выносливость - {self.unique_power}\n"
         print(info)
+ 
+    def do_attack(self,other):
+        Spec_Attac.func_attack = Hero.kick
+        Spec_Attac.do_attack(self,other)
+
 
 # эльфы(ловкость)
 class Elf(Hero, Spec_Attac):    
+
     def __init__(self, name, health, armor, strong,  unique_power, damage) -> None:
         # Hero.__init__(self, name, health, armor, strong)
         Hero.__init__(self, name, health, armor, strong)
@@ -145,14 +185,21 @@ class Elf(Hero, Spec_Attac):
     
    
     def print_info(self, sep="-"):
-        info =  f"{Hero._get_info(self)}\n"  \
+        info =  f"{__class__.__name__}\n"   \
+                f"{Hero._get_info(self)}\n"  \
                 f"{sep*20}\n" \
                 f"Ловкость - {self.unique_power}\n"
         print(info)
 
 
+    def do_attack(self,other):
+        Spec_Attac.func_attack = Hero.kick
+        Spec_Attac.do_attack(self,other)
+
 # драконы(огонь)
-class Dragon(Hero, Spec_Attac):    
+class Dragon(Hero, Spec_Attac):  
+
+
     def __init__(self, name, health, armor, strong, unique_power, damage) -> None:
         # Hero.__init__(self, name, health, armor, strong)
         Hero.__init__(self, name, health, armor, strong)
@@ -160,10 +207,15 @@ class Dragon(Hero, Spec_Attac):
         
    
     def print_info(self, sep="-"):
-        info =  f"{Hero._get_info(self)}\n"  \
+        info =  f"{__class__.__name__}\n"   \
+                f"{Hero._get_info(self)}\n"  \
                 f"{sep*20}\n" \
                 f"Огонь - {self.unique_power}\n"
         print(info)
+
+    def do_attack(self,other):
+        Spec_Attac.func_attack = Hero.kick
+        Spec_Attac.do_attack(self,other)
 
 
 
@@ -179,10 +231,16 @@ hero2.print_info()
 hero3.print_info()
 hero4.print_info()
 hero5.print_info()
+hero5.print_info()
 
 
-l = [hero1, hero2]
-print(hero1 in l)
-l.remove(hero1)
-print(l)
+
+hero2.do_attack(hero1)
+hero1.do_attack(hero3)
+hero6.do_attack(hero5)
+
+# l = [hero1, hero2]
+# print(hero1 in l)
+# l.remove(hero1)
+# print(l)
 
